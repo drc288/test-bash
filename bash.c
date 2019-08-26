c2cf2d7a8c28cffbd557f3c5edb6ff9f985849d2
Jhon112 <jhonarmont@hotmail.com>
2019-08-26 22:54:57 +0000

    Add frees to fix valgrind

--- bash.c
+++ bash.c
@@ -29,6 +29,8 @@ int main(void)
 				if ((argc == 1) && (_strcmp(argv[0], "exit") == 0))
 				{
 					fflush(stdin);
+					free_grid(argv,argc);
+					free(buf);
 					exit(1);
 				}
 
@@ -38,12 +40,14 @@ int main(void)
 					if (execve(argv[0], argv, NULL) == -1)
 						perror("./shell");
 				/*		perror("./hsh: 1: %s: not found", errno); */
-					free(str);
 					free_grid(argv, argc);
+					free(buf);
+					exit(1);
 				}
 				else
 				{
-					wait(pid);
+					free_grid(argv, argc);
+					wait(NULL);
 					kill(pid, SIGKILL);
 					/*
 					free_grid(argv, argc);
